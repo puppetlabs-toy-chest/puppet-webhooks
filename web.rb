dbd4a1cdc795bde9bf84ff360bf2716cebeb053c
Jeff McCune <jeff@puppetlabs.com>
2012-12-26 15:23:57 -0600

    Reflow the code in web.rb to match Github style
    This patch does not change the behavior of the application and instead
    simply reformats the code to match up with the Github style guidelines
    at https://github.com/styleguide/ruby

--- web.rb
+++ web.rb
@@ -21,25 +21,30 @@ post '/trello/puppet-dev-community/?' do
   pr_title = gh_data['pull_request']['title']
 
   card_name = "(PR #{repo_name}/#{pr_number}) #{pr_title}"
+  pr_num = gh_data['pull_request']['number']
+  pr_url = gh_data['pull_request']['html_url']
+  pr_body = gh_data['pull_request']['body']
   card_body = <<-EOBODY
-Links: [Pull Request #{gh_data['pull_request']['number']} Discussion](#{gh_data['pull_request']['html_url']}) and
-[File Diff](#{gh_data['pull_request']['html_url']}/files)
-#{gh_data['pull_request']['body']}
-        EOBODY
+Links: [Pull Request #{pr_number} Discussion](#{pr_url}) and
+[File Diff](#{pr_url}/files)
+#{pr_body}
+  EOBODY
 
   trello = TrelloAPI.new(app_key, secret, user_token)
-  trello.create_card(
-    :name => card_name,
-    :list => list_id,
-    :description => card_body)
+  card_options = {
+    name: card_name,
+    list: list_id,
+    description: card_body,
+  }
+  trello.create_card(card_options)
 
   "Creating card for PR #{gh_data['number']}"
 end
 
 ##
 # TrelloAPI implements behaviors specific to creating cards on the Puppet
-# Community board.  This code is based on the {RMT::Trello} class described at
-# [rmt/trello.rb](https://github.com/cprice-puppet/redmine-trello/blob/master/lib/rmt/trello.rb)
+# Community board.  This code is based on the `RMT::Trello` class described at
+# [rmt/trello.rb](http://goo.gl/sbd19)
 class TrelloAPI
   include ::Trello::Authorization
 
@@ -47,12 +52,16 @@ class TrelloAPI
     ENV.to_hash
   end
 
-  # A simple utility function for initializing authentication / authorization for the Trello REST API
+  # A simple utility function for initializing authentication / authorization
+  # for the Trello REST API
   #
-  # @param [String] the Trello App Key (can be retrieved from https://trello.com/1/appKey/generate)
-  # @param [String] the Trello "secret" (can be retrieved from https://trello.com/1/appKey/generate)
-  # @param [String] the Trello user token (can be generated with various expiration dates and
-  #   permissions via instructions at https://trello.com/docs/gettingstarted/index.html#getting-a-token-from-a-user)
+  # @param [String] app_key the Trello App Key (can be retrieved from
+  # https://trello.com/1/appKey/generate)
+  # @param [String] secret the Trello "secret" (can be retrieved from
+  # https://trello.com/1/appKey/generate)
+  # @param [String] user_token the Trello user token (can be generated with
+  # various expiration dates and permissions via instructions at
+  # https://trello.com/docs/gettingstarted/index.html#getting-a-token-from-a-user)
   def initialize(app_key, secret, user_token)
     if ::Trello::Authorization::AuthPolicy != OAuthPolicy
       ::Trello::Authorization.send :remove_const, :AuthPolicy
