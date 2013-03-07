f31b90335fa96227ed6b4f5240ff6fcb28d0d3d1
Jeff McCune <jeff@puppetlabs.com>
2013-03-06 16:46:56 -0800

    Add BaseTrelloJob#archive_card? instance method
    Without this patch jobs that speak to trello don't have an easy way to
    determine if they should archive the card or not.  This is a problem
    because we'd like to make this behavior easy for end users to configure.
    
    This patch addresses the problem by implementing the archive_card?
    instance method which reads the ARCHIVE_CARD environment variable via
    BaseTrelloJob#save_settings when the job is initially created and
    before saving it to the database.

--- spec/unit/puppet_labs/base_trello_job_spec.rb
+++ spec/unit/puppet_labs/base_trello_job_spec.rb
@@ -14,6 +14,7 @@ describe PuppetLabs::BaseTrelloJob do
         'TRELLO_SECRET' => 'sekret',
         'TRELLO_USER_TOKEN' => 'token',
         'TRELLO_TARGET_LIST_ID' => 'list_id',
+        'ARCHIVE_CARD' => 'true',
       }
     end
 
@@ -36,6 +37,57 @@ describe PuppetLabs::BaseTrelloJob do
     it "saves TRELLO_TARGET_LIST_ID as list_id" do
       subject.save_settings
       subject.list_id.should == 'list_id'
+    end
+    it "saves ARCHIVE_CARD to enable #archive_card?" do
+      subject.save_settings
+      subject.archive_card?.should be_true
+    end
+  end
+
+  describe '#archive_card?' do
+    it 'is false by default' do
+      subject.save_settings
+      subject.archive_card?.should be_false
+    end
+    it 'is false if ARCHIVE_CARD is empty' do
+      subject.env = { 'ARCHIVE_CARD' => '' }
+      subject.save_settings
+      subject.archive_card?.should be_false
+    end
+    it 'is false if ARCHIVE_CARD is garbage' do
+      subject.env = { 'ARCHIVE_CARD' => 'asdfasdf' }
+      subject.save_settings
+      subject.archive_card?.should be_false
+    end
+    it 'is true if ARCHIVE_CARD is TRUE' do
+      subject.env = { 'ARCHIVE_CARD' => 'TRUE' }
+      subject.save_settings
+      subject.archive_card?.should be_true
+    end
+    it 'is true if ARCHIVE_CARD is true' do
+      subject.env = { 'ARCHIVE_CARD' => 'true' }
+      subject.save_settings
+      subject.archive_card?.should be_true
+    end
+    it 'is true if ARCHIVE_CARD is YES' do
+      subject.env = { 'ARCHIVE_CARD' => 'YES' }
+      subject.save_settings
+      subject.archive_card?.should be_true
+    end
+    it 'is true if ARCHIVE_CARD is yes' do
+      subject.env = { 'ARCHIVE_CARD' => 'yes' }
+      subject.save_settings
+      subject.archive_card?.should be_true
+    end
+    it 'is false if ARCHIVE_CARD is YESnoYES' do
+      subject.env = { 'ARCHIVE_CARD' => 'YESnoYES' }
+      subject.save_settings
+      subject.archive_card?.should be_false
+    end
+    it 'is false if ARCHIVE_CARD is TRUEnoTRUE' do
+      subject.env = { 'ARCHIVE_CARD' => 'TRUEnoTRUE' }
+      subject.save_settings
+      subject.archive_card?.should be_false
     end
   end
 end
