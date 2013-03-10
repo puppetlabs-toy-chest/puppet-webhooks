b729f4224a042865b46deafa70136e7c36b4c9bb
razic <razic@viralkitty.com>
2013-03-10 21:45:02 +0700

    initialize a pull request when initializing a issue

--- lib/puppet_labs/issue.rb
+++ lib/puppet_labs/issue.rb
@@ -3,12 +3,13 @@ require 'json'
 # This class provides a model of a github issue.
 module PuppetLabs
 class Issue
-  # Pull request data
+  # Issue data
   attr_reader :number,
     :repo_name,
     :title,
     :html_url,
     :body,
+    :pull_request,
     :action
 
   def self.from_json(json)
@@ -29,6 +30,10 @@ class Issue
     @body = data['issue']['body']
     @repo_name = data['repository']['name']
     @action = data['action']
+    @pull_request = ::PuppetLabs::PullRequest.from_json(JSON.dump({
+      'pull_request' => data['issue']['pull_request'],
+      'repository' => data['repository']
+    }))
   end
 end
 end
