cf8d8be1349c5f030ba1c9308325e6b046a0c9be
Adrien Thebo <git@somethingsinistral.net>
2013-10-14 16:33:08 -0700

    Add preliminary support for tunable event outputs

--- lib/puppet_labs/github/pull_request_controller.rb
+++ lib/puppet_labs/github/pull_request_controller.rb
@@ -13,13 +13,37 @@ class PullRequestController < Controller
     if pull_request = options[:pull_request]
       @pull_request = pull_request
     end
+
+    @outputs = outputs_from_env
   end
+
+  attr_accessor :outputs
 
   ##
   # run processes the pull request and queues up a pull request job.
   #
   # @return [Array] containing the Sinatra route style [status, headers_hsh, body_hsh]
   def run
+    messages = {'outputs' => outputs}
+
+    jobs = []
+    if outputs.include? 'trello'
+      output = run_trello
+      messages.merge!(output)
+    end
+
+    if outputs.include? 'jira'
+      output = run_jira
+      messages.merge!(output)
+    end
+
+    return [ACCEPTED, {}, messages]
+  end
+
+  private
+
+  def run_trello
+    job = nil
     case pull_request.action
     when "opened"
       job = PuppetLabs::Trello::TrelloPullRequestJob.new
@@ -28,27 +52,41 @@ class PullRequestController < Controller
     when "closed"
       job = PuppetLabs::Trello::TrelloPullRequestClosedJob.new
     else
-      logger.info "Ignoring pull request #{pull_request.repo_name}/#{pull_request.number} because the action is #{pull_request.action}."
-      body = { 'message' => 'Action has been ignored.' }
-      return [OK, {}, body]
+      logger.info "Ignoring pull request #{pull_request.repo_name}/#{pull_request.number}: action #{pull_request.action} is unhandled"
+      return {'trello' => {'status' => 'failed', 'errors' => 'unhandled action'}}
     end
 
-    jira_job = PuppetLabs::Jira::PullRequestHandler.new
-    jira_job.pull_request = pull_request
-    jira_delayed = jira_job.queue
+    enqueue_job('trello', job)
+  end
 
-    job.pull_request = pull_request
+  def run_jira
+    job = PuppetLabs::Jira::PullRequestHandler.new
+    enqueue_job('jira', job)
+  end
+
+  def enqueue_job(name, job)
+    job.pull_request = @pull_request
     delayed_job = job.queue
-    logger.info "Successfully queued up #{job.class} (#{pull_request.repo_name}/#{pull_request.number}) as job #{delayed_job.id}"
 
-    body = {
-      'job_id' => delayed_job.id,
-      'queue' => delayed_job.queue,
-      'priority' => delayed_job.priority,
-      'created_at' => delayed_job.created_at,
+    logger.info "Queued #{job.class} (#{pull_request.repo_name}/#{pull_request.number}) as job #{delayed_job.id}"
+    {
+      name => {
+        'status'     => 'ok',
+        'job_id'     => delayed_job.id,
+        'queue'      => delayed_job.queue,
+        'priority'   => delayed_job.priority,
+        'created_at' => delayed_job.created_at,
+      }
     }
+  end
 
-    return [ACCEPTED, {}, body]
+  # Determine which event outputs should be used, based on the environment.
+  # Defaults to trello.
+  #
+  def outputs_from_env
+    str = (ENV['GITHUB_EVENT_OUTPUTS'] || 'trello')
+
+    str.split(/,/).map(&:strip)
   end
 end
 end
